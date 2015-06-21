4a2abc3b1109f95191004820b707b3022cb7e4a1
NOCH <sreynoch.soung1@gmail.com>
2015-06-21 17:57:34 +0700

    my

---
+++ resources/views/province_detail.blade.php
@@ -0,0 +1,117 @@
+@include('static.header')
+<link href="css/beach.css" rel="stylesheet" />
+<link href="css/phnom_penh.css" rel="stylesheet" />
+<link href="css/indexpp.css" rel="stylesheet" />
+
+
+<!-- start body -->
+<body>
+<div id="wrapper">
+	<section id="inner-headline">
+	<div class="container">
+		
+		<div class="row">
+			<div class="col-lg-12">
+				<ul class="breadcrumb">
+					<li><a href="#"><i class="fa fa-home"></i></a><i class="icon-angle-right"></i></li>
+					<li class="active">Blog</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	</section>
+	<!-- start center -->
+	<div id="center" class="center">
+
+
+	<!-- start content -->
+	<div id="content">
+		<div id="menudetail">
+
+			
+		       <h2>Phnom Penh</h2>
+		       <ul class="menu_l">
+		           <li class=" elevator curent">
+		           		<a href="phnom_penh" onclick="toggle('general_div');">&nbsp; &nbsp;Phnom Penh Overview<strong></strong></a>
+		           </li>
+		           <li class="elevator">
+		           		<a href="#" onclick="toggle('photo_div');">&nbsp; &nbsp;Phnom Penh Photos<strong></strong></a>
+		           </li>
+		           <li class="elevator">
+		           		<a href="#" onclick="toggle('map_div');">&nbsp; &nbsp;Phnom Penh Map<strong></strong></a>
+		           </li>
+		           <li class=" elevator">
+		           		<a href="#" onclick="toggle('tour_div');">&nbsp; &nbsp;Phnom Penh Tours<strong></strong></a>
+		           </li>
+		           <li class=" elevator">
+		           		<a href="#" onclick="toggle('hotel_div');">&nbsp; &nbsp;Phnom Penh Hotels<strong></strong></a>
+		           </li>
+		       </ul>
+			 
+		</div>
+		<div id="paradetail" class="article">
+			<h4>PHNOM PENH, PHNOM PENH TRAVEL GUIDE, PHNOM PENH CAMBODIA</h4>
+			<p>Phnom Penh, the capital of the Kingdom of Cambodia, is a charming city. Being the combination of Asian features, the elegance of 				Indochina land and the hospitality of Cambodians, Phnom Penh puts strong impression on tourists at the first look.
+             </p>
+		<div id="p_text">	
+		<div id="quick">
+			<div id="img_province">
+		        <img style="width: 390px;" src="img/province/Phnom_Penh/Phnom_Penh.jpg" title="Phnom Penh" alt="Phnom Penh">
+		    </div>
+		    <div id="quick_fact">
+		                <h2>Quick Facts:</h2>
+		                <ul>
+			                <li>
+				                <span class="nav">Location:   Located on the banks of the Mekong River</span>
+			                </li>
+			                 <li>
+				                <span class="nav">Status:     the nation's center of economic and industrial activities, the center of security, 									politics, cultural heritage, and diplomacy of Cambodia.</span>
+			                </li>
+			                 <li>
+				                <span class="nav">Population:  2,301,725 </span>
+			                </li>
+			                 <li>
+				                <span class="nav">Area:   678.46 square kilometers </span>
+			                </li>
+			                 <li>
+				                <span class="nav">Religions:  Buddhism </span>
+			                </li>
+			                 <li>
+				                <span class="nav">Known as the "Pearl of Asia”</span>
+			                </li>
+			                 <li>
+				                <span class="nav">Well-known for many beautiful and historical architecture and attractions</span>
+			                </li>
+			                 <li>
+				                <span class="nav">The wealthiest and most populous city in Cambodia and is the country's political hub.</span>
+			                </li>
+		                </ul>
+	                
+	                </div>
+		</div>	
+	</div>
+	
+                <p>
+	                
+	Located on the meeting points of three big rivers: Bassac, Mekong and Tonle Sap, Phnom Penh is also called as “four-river-surrounded city” (Chatomuk). Phnom Penh is not also the capital but also political, economic and cultural center of Cambodia.
+                </p>
+				<p>
+					
+	With the total are of about 678.46 square kilometers and the population of about 2 millions people, Phnom Penh is the largest city in Cambodia. It is a nice French-built city with long history since 1434. This city is named after the present hill temple (Wat Phnom) and a lady who is thought to be the founder of this land – Lady Penh.
+				</p>
+				<p>
+					
+	Phnom Penh is the gate way to many holy lands of Cambodia: Angkor Complex in the West, white sand beaches of Sihanouk Villa in the South, Rattanakiri and Mondolkiri forest in the Northeast, and a variety of unexploited lands, remote villages and ruined temples around the country. All places are waiting for tourists’ exploration.
+				</p>
+        	 </div>
+	</div>
+		
+	<!--end content-->
+
+
+	
+	
+</div>
+</body>
+<!-- end body -->
+@include('static.footer')
---
+++ resources/views/provinces.blade.php
@@ -0,0 +1,389 @@
+@include('static.header')
+<body>
+<div id="wrapper">
+	<section id="inner-headline">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<ul class="breadcrumb">
+					<li><a href="#"><i class="fa fa-home"></i></a><i class="icon-angle-right"></i></li>
+					<li class="active">Provinces</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	</section>
+	<section id="content">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+					<article>
+					
+				<h1>ALL PROVINCES</h1>
+				</div>
+
+				</article>
+				<div>
+				
+
+				<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+						<img  src="img/Province/Phnom_Penh/1.jpg" alt=""/>
+
+					</div>
+					<h4>Phnom Penh</h4>
+				<p>
+					 Phnom Penh, the capital of the Kingdom of Cambodia, is a charming city. Being the combination of...
+				</p>
+				</div>
+
+				</article>
+				<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+						<img src="img/Province/Siem_Reap/1.jpg" alt=""/>
+
+					</div>
+					<h4>Siem Reap</h4>
+				<p>
+					 Located the northwest of Cambodia about 300km from Phnom Penh capital, Siem Reap is an invaluable...
+				</p>
+				</div>
+
+				</article>
+				<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+						<img src="img/Province/Preah_Vihear/1.jpg" alt=""/> 
+					</div>
+				<h4>Preah Vihear</h4>
+				<p>
+					 This northeast province of Cambodia is approximately 543 km far from the capital city of Phnom...
+				</p>
+				</div>
+
+				</article>
+				<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+						<img src="img/Province/Koh_Kong/1.jpg" alt=""/>
+
+					</div>
+				<h4>Koh Kong</h4>
+				<p>
+					 Being the largest island of Cambodia, Koh Kong is just 25km to the south of Krong Koh Kong. The...
+				</p>
+				</div>
+
+				</article>
+
+				<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+						<img src="img/Province/Preah_Sihanouk/1.jpg" alt=""/>
+
+					</div>
+				<h4>Preah Sihanouk</h4>
+				<p>
+					Lying in the land with thousands years of history, Sihanoukville is listed among the most beautiful...
+				</p>
+				</div>
+
+				</article>
+				<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+					
+				<img src="img/Province/Kep/1.jpg" alt=""/>
+					</div>
+				<h4>Kep</h4>
+				<p>
+					Back to 1960s, Kep was a famous coastal city, was a highlight destination during holidays for...
+				</p>
+				</div>
+
+				</article>
+
+				<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+						<img src="img/Province/Battambong/1.jpg" alt="" />
+					</div>
+				<h4>Battambong</h4>
+				<p>
+					 Located in the northwestern of Cambodia, Battambang is famous for being the rice basket of...
+				</p>
+				</div>
+
+				</article>
+				<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+					<img src="img/Province/Rattanakiri/1.jpg" alt="" />
+				</div>
+				<h4>Rattanankiri</h4>
+				<p>
+					 is the "Wild East" of Cambodia, situated on the mountainous within Cambodia northeastern corner of Cambodia...
+				</p>
+				</div>
+
+				</article>
+
+
+
+				</article>
+				<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+					<img src="img/Province/Mondulkiri/1.jpg" alt="" />
+				</div>
+				<h4>Mondulkiri</h4>
+				<p>
+					 Located 543 Km northeast of Phnom Penh city. The province has a total land area 14,288 square kilometers...
+				</p>
+				</div>
+
+				</article>
+
+
+				</article>
+				<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+				<img src="img/Province/Kampot/1.jpg" alt="" />
+				</div>
+				<h4>Kampot</h4>
+				<p>
+					Located in the Southwest of the country, having an eighty kilometres coastal strip with the Gulf of Thailand...
+				</p>
+				</div>
+
+				</article>
+
+
+				</article>
+				<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+					<img src="img/Province/Oddar_Meanchey/1.jpg" alt="" />
+				</div>
+				<h4>Oddar Meanchey</h4>
+				<p>
+					is one of the smallest provinces of Cambodia located in the far Northwest bordering with Thailand ...
+				</p>
+				</div>
+
+				</article>
+
+
+				</article>
+				<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+				<img src="img/Province/Banteay_Meanchey/1.jpg" alt="" />
+				</div>
+				<h4>Banteay Meanchey</h4>
+			
+				<p>
+					 Located in the far northwest. It borders the provinces of Oddar Meanchey and Siem Reap to the east...
+				</p>
+				</div>
+
+				</article>
+
+
+
+
+					<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+				
+				<img src="img/Province/Kandal/1.jpg" alt="" />
+			</div>
+				<h4>Kandal</h4>
+				<p>
+					 Located in the southeast portion of the country. It borders the provinces of Prey Veng and Takeo...
+				</p>
+				</div>
+
+				</article>
+
+					<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+			
+				<img src="img/Province/Stung_Treng/1.jpg" alt="" />
+			</div>
+				<h4>Stung Treng</h4>
+				<p>
+					 Located in the northeast. It borders the provinces of Ratanakiri to the east, Mondulkiri and Kratie....
+				</p>
+				</div>
+
+				</article>
+
+					<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+					<img src="img/Province/Takeo/1.jpg" alt=""/>
+				</div>
+				<h4>Takeo</h4>
+				<p>
+					 Located in the southwest of Cambodia, Takeo borders the provinces of Kampot to the west, Kampong Speu to...
+				</p>
+				</div>
+
+				</article>
+
+					<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+				
+				<img src="img/Province/kampong_Speu/1.jpg" alt="" />
+			</div>
+				<h4>Kampong Speu</h4>
+				<p>
+					 Kampong Speu's borders the provinces of Pursat and Kampong Chhnang to the north, Kandal to the east...
+				</p>
+				</div>
+
+				</article>
+
+
+
+					<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+				<img src="img/Province/Prey_Veng/1.jpg" alt=""/>
+			</div>
+				<h4>Prey Veng</h4>
+				<p>
+					 In 2008 it had a total population of 947,357 people. This densely populated agricultural region is located on the east bank...
+				</p>
+				</div>
+
+				</article>
+					<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+				
+				<img src="img/Province/Svay_Rieng/1.jpg" alt=""/>
+			</div>
+				<h4>Svay Rieng</h4>
+				<p>
+					 Located in the southeast, the province juts into Vietnam, which surrounds it to the north, east and south...
+				</p>
+				</div>
+
+				</article>
+					<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+				
+				<img src="img/Province/Kratie/1.jpg" alt="" />
+				<</div>
+				<h4>Kratie</h4>
+				<p>
+					 Located in the northeast. It borders Stung Treng to the north, Mondulkiri to the east, Kampong Thom...
+				</p>
+				</div>
+
+				</article>
+					<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+				
+				<img src="img/Province/Kampong_Chhnang/1.jpg" alt=""/>
+			</div>
+				<h4>Kampong Chhnang</h4>
+				<p>
+					 Located in the West bank of the Tonle Sap Lake at the heartland of Cambodia. Neighboring provinces are Kampong Thom...
+				</p>
+				</div>
+
+				</article>
+					<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+				
+			
+				<img src="img/Province/Pailin/1.jpg" alt="" />
+			</div>
+				<h4>Pailin</h4>
+				<p>
+					 Is a province in western Cambodia at the northern edge of the Cardamom Mountains near the border of Thailand...
+				</p>
+				</div>
+
+				</article>
+					<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+				
+			
+				<img src="img/Province/Kampong_Cham/1.jpg" alt=""/>
+			</div>
+				<h4>Kampong Cham</h4>
+				<p>	 
+					Being the third largest city in Cambodia, Kampong Cham is the third largest city in Cambodia. For...
+				</p>
+				</div>
+
+				</article>
+					<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+				<img src="img/Province/Kampong_Thom/1.jpg" alt=""/>
+			</div>
+				<h4>Kampong Thom</h4>
+				<p>
+					Located at the centre of Cambodia, Kampong Thom is the second largest province of this kingdom....
+				</p>
+				</div>
+
+				</article>
+					<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+				<img src="img/Province/Pursat/1.jpg" alt=""/>
+			</div>
+				<h4>Pursat</h4>
+				<p>
+					Located in the western part of the country and borders clockwise from the north with Battambang Province...
+				</p>
+				</div>
+
+				</article>
+				<article>
+					<div class="col-lg-3">
+					<div class="post-image">
+				
+				<img src="img/Province/Tbong_Khmum/1.jpg" alt="" />
+			</div>
+				<h4>Tbong Khmum</h4>
+				<p>
+					Located on the central lowlands of the Mekong River. It borders the provinces of Kampong Cham to the west...
+				</p>
+				</div>
+
+				</article>
+				</div>
+		
+				<!-- end flexslider -->
+				
+		</div>
+		
+
+
+				
+			</article>
+		
+		</div>
+	</div>
+	</section>
+	
+</div>
+
+</body>
+@include('static.footer')
