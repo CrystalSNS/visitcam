aa21ebb23b69b72954a6ce9579be295129047137
NOCH <sreynoch.soung1@gmail.com>
2015-06-23 16:32:06 +0700

    menu travel guide

--- resources/views/static/menu_travel.blade.php
+++ resources/views/static/menu_travel.blade.php
@@ -1,38 +1,22 @@
-<footer>
-	<div id="sub-footer">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-6">
-					<div class="copyright">
-						<p>
-							<span>&copy; 2014 ITC|GIC|I4|by Group 8 </span><a href="" target="_blank"></a>
-						</p>
-					</div>
-				</div>
-				<div class="col-lg-6">
-					<ul class="social-network">
-						<li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-						<li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-						<li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus"></i></a></li>
-					</ul>
-				</div>
-			</div>
+<div class="col-lg-3">
+	<aside class="right-sidebar">
+		<div class="widget">
+			<form class="form-search">
+				<input class="form-control" type="text" placeholder="Search..">
+			</form>
 		</div>
-	</div>
-</footer>
+		<div class="widget">
+			<h5 class="widgetheading">Travel Guide</h5>
+			<ul class="cat">
+				<li><i class="icon-angle-right"></i><a href="geography_weather">Geography and Weather</a></li>
+				<li><i class="icon-angle-right"></i><a href="history">History</a></li>
+				<li><i class="icon-angle-right"></i><a href="religion">Religion</a></li>
+				<li><i class="icon-angle-right"></i><a href="traditional_costume">Traditional Costume</a></li>
+				<li><i class="icon-angle-right"></i><a href="festival_event">Festival and Event</a></li>
+				<li><i class="icon-angle-right"></i><a href="currency">Currency</a></li>
+				<li><i class="icon-angle-right"></i><a href="transportation">Transportation</a></li>
+			</ul>
+		</div>
 
-<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
-<!-- javascript
-    ================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="js/jquery.js"></script>
-<script src="js/jquery.easing.1.3.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/jquery.fancybox.pack.js"></script>
-<script src="js/jquery.fancybox-media.js"></script>
-<script src="js/google-code-prettify/prettify.js"></script>
-<script src="js/portfolio/jquery.quicksand.js"></script>
-<script src="js/portfolio/setting.js"></script>
-<script src="js/jquery.flexslider.js"></script>
-<script src="js/animate.js"></script>
-<script src="js/custom.js"></script>
+	</aside>
+</div>
