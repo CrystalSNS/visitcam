358002a2c78374b23fab0452be947d7eeb5b9f01
NOCH <sreynoch.soung1@gmail.com>
2015-07-02 17:40:32 +0700

    province detail

--- resources/views/province_detail.blade.php
+++ resources/views/province_detail.blade.php
@@ -1,119 +1,84 @@
-
 @include('static.header')
-<link href="css/beach.css" rel="stylesheet" />
 <link href="css/phnom_penh.css" rel="stylesheet" />
-<link href="css/indexpp.css" rel="stylesheet" />
-
-
-<!-- start body -->
 <body>
 <div id="wrapper">
 	<section id="inner-headline">
-	<div class="container">
-		
-		<div class="row">
-			<div class="col-lg-12">
-				<ul class="breadcrumb">
-					<li><a href="#"><i class="fa fa-home"></i></a><i class="icon-angle-right"></i></li>
-					<li class="active">Blog</li>
-				</ul>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<ul class="breadcrumb">
+						<li><a href="#"><i class="fa fa-home"></i></a><i class="icon-angle-right"></i></li>
+						<li class="active">Phnom Penh</li>
+					</ul>
+				</div>
 			</div>
 		</div>
-	</div>
 	</section>
-	<!-- start center -->
-	<div id="center" class="center">
-
-
-	<!-- start content -->
-	<div id="content">
-		<div id="menudetail">
-
-			
-		       <h2>Phnom Penh</h2>
-		       <ul class="menu_l">
-		           <li class=" elevator curent">
-		           		<a href="phnom_penh" onclick="toggle('general_div');">&nbsp; &nbsp;Phnom Penh Overview<strong></strong></a>
-		           </li>
-		           <li class="elevator">
-		           		<a href="#" onclick="toggle('photo_div');">&nbsp; &nbsp;Phnom Penh Photos<strong></strong></a>
-		           </li>
-		           <li class="elevator">
-		           		<a href="#" onclick="toggle('map_div');">&nbsp; &nbsp;Phnom Penh Map<strong></strong></a>
-		           </li>
-		           <li class=" elevator">
-		           		<a href="#" onclick="toggle('tour_div');">&nbsp; &nbsp;Phnom Penh Tours<strong></strong></a>
-		           </li>
-		           <li class=" elevator">
-		           		<a href="#" onclick="toggle('hotel_div');">&nbsp; &nbsp;Phnom Penh Hotels<strong></strong></a>
-		           </li>
-		       </ul>
-			 
+	<section id="content">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-9">
+					@foreach($provincedetail as $prodetail)
+						<article>
+							<div class="col-lg-3" id="para">
+								<h3>{{$prodetail['ov_title']}}</h3><br/>
+								<p>{{$prodetail['ov_head_text']}}</p><br/>
+								<img style="width: 370px;"  src={{$prodetail['ov_img_url']}} alt=""/>
+								<div class="col-lg-3" id="quick_fact">
+									<h2>Quick Facts:</h2>
+									<ul>
+										<li>
+											<span class="nav">Location:  {{$prodetail['ov_location']}}</span>
+										</li>
+										<li>
+											<span class="nav">Status:  {{$prodetail['ov_status']}}</span>
+										</li>
+										<li>
+											<span class="nav">Population:  {{$prodetail['ov_population']}}</span>
+										</li>
+										<li>
+											<span class="nav">Area:  {{$prodetail['ov_area']}}</span>
+										</li>
+										<li>
+											<span class="nav">Religions:  {{$prodetail['ov_religion']}}</span>
+										</li>
+										<li>
+											<span class="nav">Known as  {{$prodetail['ov_known']}}</span>
+										</li>
+										<li>
+											<span class="nav">Well-known  {{$prodetail['ov_wellknown']}}</span>
+										</li>
+										<li>
+											<span class="nav">  {{$prodetail['ov_condition']}}</span>
+										</li>
+								</div>
+								<<p>{{$prodetail['ov_bottom_text']}}</p>
+							</div>
+						</article>
+					@endforeach
+				</div>
+				<div class="col-lg-3">
+					<aside class="right-sidebar">
+						<div class="widget">
+							<form class="form-search">
+								<input class="form-control" type="text" placeholder="Search..">
+							</form>
+						</div>
+						<div class="widget">
+							<h5 class="widgetheading"><h3>Phnom Penh</h3></h5>
+							<ul class="cat">
+								<li><i class="icon-angle-right"></i><a href="province_detail">Phnom Penh Overview</a></li>
+								<li><i class="icon-angle-right"></i><a href="history">Phnom Penh Photos</a></li>
+								<li><i class="icon-angle-right"></i><a href="religion">Phnom Penh Map</a></li>
+								<li><i class="icon-angle-right"></i><a href="traditional_costume">Phnom Penh Tours</a></li>
+								<li><i class="icon-angle-right"></i><a href="festival_event">Phnom Penh Hotels</a></li>
+							</ul>
+						</div>
+					</aside>
+				</div>
+			</div>
 		</div>
-		<div id="paradetail" class="article">
-			<h4>PHNOM PENH, PHNOM PENH TRAVEL GUIDE, PHNOM PENH CAMBODIA</h4>
-			<p>Phnom Penh, the capital of the Kingdom of Cambodia, is a charming city. Being the combination of Asian features, the elegance of 				Indochina land and the hospitality of Cambodians, Phnom Penh puts strong impression on tourists at the first look.
-             </p>
-		<div id="p_text">	
-		<div id="quick">
-			<div id="img_province">
-		        <img style="width: 390px;" src="img/province/Phnom_Penh/Phnom_Penh.jpg" title="Phnom Penh" alt="Phnom Penh">
-		    </div>
-		    <div id="quick_fact">
-		                <h2>Quick Facts:</h2>
-		                <ul>
-			                <li>
-				                <span class="nav">Location:   Located on the banks of the Mekong River</span>
-			                </li>
-			                 <li>
-				                <span class="nav">Status:     the nation's center of economic and industrial activities, the center of security, 									politics, cultural heritage, and diplomacy of Cambodia.</span>
-			                </li>
-			                 <li>
-				                <span class="nav">Population:  2,301,725 </span>
-			                </li>
-			                 <li>
-				                <span class="nav">Area:   678.46 square kilometers </span>
-			                </li>
-			                 <li>
-				                <span class="nav">Religions:  Buddhism </span>
-			                </li>
-			                 <li>
-				                <span class="nav">Known as the "Pearl of Asia”</span>
-			                </li>
-			                 <li>
-				                <span class="nav">Well-known for many beautiful and historical architecture and attractions</span>
-			                </li>
-			                 <li>
-				                <span class="nav">The wealthiest and most populous city in Cambodia and is the country's political hub.</span>
-			                </li>
-		                </ul>
-	                
-	                </div>
-		</div>	
-	</div>
-	
-                <p>
-	                
-	Located on the meeting points of three big rivers: Bassac, Mekong and Tonle Sap, Phnom Penh is also called as “four-river-surrounded city” (Chatomuk). Phnom Penh is not also the capital but also political, economic and cultural center of Cambodia.
-                </p>
-				<p>
-					
-	With the total are of about 678.46 square kilometers and the population of about 2 millions people, Phnom Penh is the largest city in Cambodia. It is a nice French-built city with long history since 1434. This city is named after the present hill temple (Wat Phnom) and a lady who is thought to be the founder of this land – Lady Penh.
-				</p>
-				<p>
-					
-	Phnom Penh is the gate way to many holy lands of Cambodia: Angkor Complex in the West, white sand beaches of Sihanouk Villa in the South, Rattanakiri and Mondolkiri forest in the Northeast, and a variety of unexploited lands, remote villages and ruined temples around the country. All places are waiting for tourists’ exploration.
-				</p>
-        	 </div>
-	</div>
-		
-	<!--end content-->
-
-
-	
-	
+	</section>
 </div>
 </body>
-<!-- end body -->
-
 @include('static.footer')
