191f486d573e33f105c940156f15288cde117041
NOCH <sreynoch.soung1@gmail.com>
2015-07-02 19:05:26 +0700

    i edit province detail

--- app/Province_detailModel.php
+++ app/Province_detailModel.php
@@ -8,7 +8,11 @@ class Province_detailModel extends Model {
 	protected $table='prooverviews';
 	public static function getPro_detail($id)
 	{
-		return Province_detailModel::where ('province_id', '=', $id)->get();
+		return Province_detailModel::join('provinces','prooverviews.province_id','=','provinces.pro_id')
+			->where('prooverviews.province_id','=',$id)
+			->select('prooverviews.*','provinces.pro_name')
+			->get();
+		//return Province_detailModel::where ('province_id', '=', $id)->get();
 	}
 
 
