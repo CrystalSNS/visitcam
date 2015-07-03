244191baa372fe83c706c8343261135c5b68aee3
seren123 <seren.itc@gmail.com>
2015-07-04 00:26:42 +0700

    modified index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,162 +19,144 @@
 		<li><a href="contact_us">Contact</a></li>
 	</ul>
 </div>
-</div>
-</div>
-
-</header>
 <body>
-
+</header>
 <div id="wrapper">
 	<section id="featured">
-	<!-- start slider -->
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-12">
-	<!-- Slider -->
-
-        <div id="main-slider" class="flexslider">
-        	
-            <ul class="slides">
-            	@foreach($indexObject['slide'] as $slideshow)
-              <li>
-                <img src="{{$slideshow['slide_img']}}" alt="" />
-                <div class="flex-caption">
-					<p>{{$slideshow['slide_title']}}</p>
-                </div>
-              </li>
-            
-				@endforeach
-            </ul>
-            
-        </div>
-	<!-- end slider -->
-		
-		<!-- end divider -->
-		<!-- Portfolio Projects -->
-		<div class="row">
-			<div class="col-lg-12">
-				<h3 class="heading">Popular History Place</h3>
-				
-					
-				
-				<div class="row">
-					<section id="projects">
-					<ul id="thumbs" class="portfolio">
-						@foreach($indexObject['history'] as $historypl)
-						<!-- Item Project and Filter Name -->
-						<li class="col-lg-3 design" data-id="id-0" data-type="web">
-						<div class="item-thumbs">
-						<!-- Fancybox - Gallery Enabled - Title - Full Image -->
-						<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="{{$historypl['pl_name']}}" href="{{$historypl['pl_img_url']}}">
-						<span class="overlay-img"></span>
-						<span class="overlay-img-thumb font-icon-plus"></span>
-						</a>
-						<!-- Thumb Image and Description -->
-						<img src="{{$historypl['pl_img_url']}}" alt="{{$historypl['pl_name']}}">
+		<!-- start slider -->
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<!-- Slider -->
+					<div id="main-slider" class="flexslider">
+						<ul class="slides">
+							@foreach($indexObject['slide'] as $slideshow)
+							<li>
+								<img src="{{$slideshow['slide_img']}}" alt="" />
+								<div class="flex-caption">
+									<p>{{$slideshow['slide_title']}}</p>
+								</div>
+							</li>
+							@endforeach
+						</ul>
+					</div>
+					<!-- end divider -->
+					<!-- Portfolio Projects -->
+					<div class="row">
+						<div class="col-lg-12">
+							<h3 class="heading">Popular History Place</h3>
+							<div class="row">
+								<section id="projects">
+									<ul>
+										@foreach($indexObject['history'] as $historypl)
+										<!-- Item Project and Filter Name -->
+										<li class="col-lg-3 design" data-id="id-0" data-type="web">
+											<div class="item-thumbs">
+												<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+												<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="{{$historypl['pl_name']}}" href="{{$historypl['pl_img_url']}}">
+													<span class="overlay-img"></span>
+													<span class="overlay-img-thumb font-icon-plus"></span>
+												</a>
+												<!-- Thumb Image and Description -->
+												<img src="{{$historypl['pl_img_url']}}" alt="{{$historypl['pl_name']}}">
+											</div>
+										</li>
+										@endforeach
+										<!-- End Item Project -->
+									</ul>
+								</section>
+							</div>
 						</div>
-						</li>
-
-						@endforeach
-						<!-- End Item Project -->
-						
-
-					</ul>
-					</section>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-lg-12">
-				<h3 class="heading">Beautiful Beach in Cambodia</h3>
-				<div class="row">
-					<section id="projects">
-						<ul>
-						@foreach($indexObject['beach'] as $beachpl)
-						<!-- Item Project and Filter Name -->
-						<li class="col-lg-3 design" data-id="id-0" data-type="web">
-						<div class="item-thumbs">
-						<!-- Fancybox - Gallery Enabled - Title - Full Image -->
-						<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="{{$beachpl['pl_name']}}" href="{{$beachpl['pl_img_url']}}">
-						<span class="overlay-img"></span>
-						<span class="overlay-img-thumb font-icon-plus"></span>
-						</a>
-						<!-- Thumb Image and Description -->
-						<img src="{{$beachpl['pl_img_url']}}" alt="{{$beachpl['pl_name']}}">
+					</div>
+					<div class="row">
+						<div class="col-lg-12">
+							<h3 class="heading">Beautiful Beach in Cambodia</h3>
+							<div class="row">
+								<section id="projects">
+									<ul>
+										@foreach($indexObject['beach'] as $beachpl)
+										<!-- Item Project and Filter Name -->
+										<li class="col-lg-3 design" data-id="id-0" data-type="web">
+											<div class="item-thumbs">
+												<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+												<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="{{$beachpl['pl_name']}}" href="{{$beachpl['pl_img_url']}}">
+													<span class="overlay-img"></span>
+													<span class="overlay-img-thumb font-icon-plus"></span>
+												</a>
+												<!-- Thumb Image and Description -->
+												<img src="{{$beachpl['pl_img_url']}}" alt="{{$beachpl['pl_name']}}">
+											</div>
+										</li>
+										@endforeach
+										<!-- End Item Project -->
+									</ul>
+								</section>
+							</div>
 						</div>
-						</li>
+					</div>
+					<div class="row">
+						<div class="col-lg-12">
+							<h3 class="heading">Popular Overland Province</h3>
+							<div class="row">
+								<section id="projects">
 
-						@endforeach
-						<!-- End Item Project -->
-					</ul>
-					</section>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-lg-12">
-				<h3 class="heading">Popular Overland Province</h3>
-				<div class="row">
-					<section id="projects">
-
-					<ul id="thumbs" class="portfolio">
-						@foreach($indexObject['overland'] as $overlanvpl)
-						<!-- Item Project and Filter Name -->
-						<li class="col-lg-3 design" data-id="id-0" data-type="web">
-						<div class="item-thumbs">
-						<!-- Fancybox - Gallery Enabled - Title - Full Image -->
-						<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="{{$overlanvpl['pl_name']}}" href="{{$overlanvpl['pl_img_url']}}">
-						<span class="overlay-img"></span>
-						<span class="overlay-img-thumb font-icon-plus"></span>
-						</a>
-						<!-- Thumb Image and Description -->
-						<img src="{{$overlanvpl['pl_img_url']}}" alt="{{$overlanvpl['pl_name']}}">
+									<ul id="thumbs" class="portfolio">
+										@foreach($indexObject['overland'] as $overlanvpl)
+										<!-- Item Project and Filter Name -->
+										<li class="col-lg-3 design" data-id="id-0" data-type="web">
+											<div class="item-thumbs">
+												<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+												<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="{{$overlanvpl['pl_name']}}" href="{{$overlanvpl['pl_img_url']}}">
+													<span class="overlay-img"></span>
+													<span class="overlay-img-thumb font-icon-plus"></span>
+												</a>
+												<!-- Thumb Image and Description -->
+												<img src="{{$overlanvpl['pl_img_url']}}" alt="{{$overlanvpl['pl_name']}}">
+											</div>
+										</li>
+										@endforeach
+										<!-- End Item Project -->
+									</ul>
+								</section>
+							</div>
 						</div>
-						</li>
-
-						@endforeach
-						<!-- End Item Project -->
-					</ul>
-					</section>
+					</div>
 				</div>
-			</div>
+			</section>
+
+
 		</div>
-
-
-
-	</div>
 	</section>
 
 	<script type="text/javascript">
 
-		var make_button_active = function()
-		{
-			alert("YES");
+	var make_button_active = function()
+	{
+		alert("YES");
 			//Get item siblings
 			var siblings =($(this).siblings());
 
 			//Remove active class on all buttons
 			siblings.each(function (index)
-					{
-						$(this).removeClass('active');
-					}
+			{
+				$(this).removeClass('active');
+			}
 			)
-
 
 			//Add the clicked button class
 			$(this).addClass('active');
-		}
+	}
 
 		//Attach events to menu
 		$(document).ready(
-				function()
-				{
-					alert("YES");
+			function()
+			{
+				alert("YES");
 
-					$(".nav li").click(make_button_active);
-				}
-		)
+				$(".nav li").click(make_button_active);
+			}
+			)
 
-	</script>
-</body>
-@include('static.footer')
+		</script>
+	</body>
+	@include('static.footer')
